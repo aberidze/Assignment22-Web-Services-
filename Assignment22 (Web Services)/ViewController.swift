59b02eb3e8b08e83325e19832df9664dc265d9ab
Anri_Beridze <aberidze02@gmail.com>
2023-11-19 13:53:41 +0400

    Initial Commit

---
+++ Assignment22 (Web Services)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment22 (Web Services)
+//
+//  Created by Macbook Air 13 on 19.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
